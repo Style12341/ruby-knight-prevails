54b01f58f424eec76b5c4cbcf226ad42f92f9b84
Maximiliano Bernard <bmaximiliano0210@gmail.com>
2024-03-25 15:30:04 -0300

    Refactor to class

--- knight_prevails.rb
+++ knight_prevails.rb
@@ -1,32 +1,60 @@
-$visited_squares = Array.new(8) { Array.new(8, false) }
-$best_ans = Hash.new(nil)
-# Both pairs
-def in_bounds?(row, col)
-  row.between?(0, 7) && col.between?(0, 7)
-end
 
-def get_possible_moves(pos)
-  [[pos[0] + 1, pos[1] + 2], [pos[0] - 1, pos[1] + 2], [pos[0] + 2, pos[1] + 1], [pos[0] + 2, pos[1] - 1], [pos[0] + 1, pos[1] - 2],
-   [pos[0] - 1, pos[1] - 2], [pos[0] - 2, pos[1] + 1], [pos[0] - 2, pos[1] - 1]]
-end
-
-def knight_prevails(start, last)
-  start_row = start[0]
-  start_col = start[1]
-  return [] unless in_bounds?(start_row, start_col)
-  return [last] if start == last
-  return $best_ans[start] if $best_ans[start]
-  return [] if $visited_squares[start_row][start_col]
-
-  $visited_squares[start_row][start_col] = true
-  ans = Array.new(200)
-  get_possible_moves(start).each do |pair|
-    aux = knight_prevails(pair, last)
-    p 'help' if start == [2, 4] and pair == [4, 3]
-    ans = [start] + aux if aux.length + 1 < ans.length && aux[-1] == last
+class KnightPuzzle
+  def initialize
+    @transformations = [[1, 2], [-1, 2], [2, 1], [2, 1], [1, - 2], [- 1, - 2], [- 2, 1], [- 2, - 1]]
+    @visited_squares = Array.new(8) { Array.new(8, false) }
+    @best_ans = Hash.new(nil)
   end
-  $best_ans[start] = ans
-  ans
+
+  def knight_prevails(start, last)
+    return [last] if start == last
+    return ans?(start) if ans?(start)
+    return [] if visited?(start)
+
+    visit(start)
+    ans = Array.new(50)
+    get_moves(start).each do |pair|
+      curr_ans = knight_prevails(pair, last)
+      ans = [start] + curr_ans if curr_ans.length + 1 < ans.length && curr_ans[-1] == last
+    end
+    set_best_ans(start, ans)
+    ans
+  end
+
+  private
+
+  def set_best_ans(pos, arr)
+    @best_ans[pos] = arr
+  end
+
+  def visit(pos)
+    @visited_squares[pos[0]][pos[1]] = true
+  end
+
+  def ans?(pos)
+    @best_ans[pos]
+  end
+
+  def visited?(pos)
+    @visited_squares[pos[0]][pos[1]]
+  end
+
+  def get_moves(pos)
+    ans = []
+    @transformations.each do |trans|
+      ans << sum_pos(pos, trans) if in_bounds?(sum_pos(pos, trans))
+    end
+    ans
+  end
+
+  def sum_pos(pos1, pos2)
+    [pos1[0] + pos2[0], pos1[1] + pos2[1]]
+  end
+
+  def in_bounds?(pos)
+    pos[0].between?(0, 7) && pos[1].between?(0, 7)
+  end
 end
 
-p knight_prevails([0, 0], [7, 7])
+kp = KnightPuzzle.new
+p kp.knight_prevails([3, 3], [4, 3])
